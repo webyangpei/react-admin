ee171173fdebc884df3f59d640c3a771863ed54c
webyangpei <1208812362@qq.com>
2020-08-13 08:12:17 +0800

    feat: add router cinfig

---
+++ src/route/config.js
@@ -0,0 +1,18 @@
+const menu = [
+  {
+    name: 'dashboard',
+    path: '/',
+    icon: 'setting',
+    title: '首页',
+    component: 'Dashboard'
+  },
+  {
+    name: 'login',
+    path: '/login',
+    icon: 'login',
+    title: '首页',
+    component: 'Login'
+  }
+]
+
+export default menu;
--- src/route/index.js
+++ src/route/index.js
@@ -1,1 +1,27 @@
-import { Swtich, Route, BrowserRouter, HashHistory, Link  } from 'react-router-dom';
+import React from 'react';
+import { Switch, Route, Redirect, BrowserRouter  } from 'react-router-dom';
+import menu from './config'
+
+const RouterView = (props) => {
+  const createMenu = (route) => {
+    return (
+      <Route
+        key={route.path}
+        path={route.path}
+        exact
+        render={ () =>  route.component}
+      >
+      </Route>
+    )
+  }
+  return (
+    <BrowserRouter>
+      <Switch>
+        {menu.map(createMenu)}
+        <Route render={() => <Redirect to='/404'></Redirect> }></Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default RouterView;
