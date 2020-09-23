6e0a48cd30b66f90e5bf75e14e1f17f71c8e0df4
wb_yangpei <wb_yangpei@kuaishou.com>
2020-09-23 11:13:16 +0800

    feat: 增加环境变量； 增加代理配置

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { render } from 'react-dom';
 import React from 'react';
 import App from './App'
-// import global scss
 import './styles/index.scss';
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
-// import zhCN from 'antd/es/locale/zh_CN';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
 import 'antd/dist/antd.css';
 moment.locale('zh-cn');
 
-render(<App/>, document.getElementById('root'))
+render(<App/>, document.getElementById('root'));
