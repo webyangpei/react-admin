bd4777871d6222eb5a369bf095de8eea026aea33
webyangpei <webyangpei@gmail.com>
2020-09-24 22:48:17 +0800

    update code

--- src/views/login.js
+++ src/views/login.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as API_User from '@/api/user.js';
 
-class Login extends React.Component {
-  render() {
-    return (
-      <div className="main-content">this is a login page!</div>
-    )
-  }
+
+const Login = () => {
+  const { UserInfo, setUserInfo } = useState([]);
+
+  useEffect(() => {
+    API_User.login().then(res => {
+      console.log(res, 'test')
+      setUserInfo(res || [])
+    })
+  },[]);
+
+  return (
+    <div className="main-content">this is a login page!</div>
+  )
 }
 
 export default Login;
